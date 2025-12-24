97120d26d1d8a091118d05bff16a4e7d32eba85d
phamtsang <ptsang2611@gmail.com>
2025-12-25 01:25:55 +0700

    Revert "Chức năng admmin: QL gói, QL owner, báo cáo tổng"
    This reverts commit 48f012d326b0a3b81400cab0d14e34559c44c579.

--- backend/admin/demo/src/main/java/com/example/demo/infrastructure/repository/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.infrastructure.repository;
-
-import com.example.demo.domain.entity.Store;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StoreRepository extends JpaRepository<Store, Long> {
-}
